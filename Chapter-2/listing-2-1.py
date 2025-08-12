0b2764b062201a61bb106c713d462def58324cf5
Niarfe <efrain.olivares@gmail.com>
2025-08-11 20:42:53 -0700

    add makefile

--- Chapter-2/listing-2-1.py
+++ Chapter-2/listing-2-1.py
@@ -1,18 +1,24 @@
+"""
+#% BASE example.  " 3D " plotting of data.  Reqs: df, fig, layout
+"""
+
 from dash import Dash, dcc, html
 import plotly.express as px
 import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
+
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Fruit":  ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+    "City":   ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
+
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
     html.Div(children='''
@@ -24,5 +30,6 @@ app.layout = html.Div(children=[
         figure=fig
     )
 ])
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
